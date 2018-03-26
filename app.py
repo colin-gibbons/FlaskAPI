eede780697996d3b50c0540020cbd7bb08e6ecf8
colin-gibbons <colingibbons@outlook.com>
2018-03-26 00:46:54 -0500

    formatting

--- app.py
+++ app.py
@@ -8,33 +8,33 @@ tasks = [ # add your tasks here
     {
         'id':'md5',
         'title': "MD5",
-        'description': 'Return md5 hash.',
+        'description': 'Returns an md5 hash of the passed string.',
         'done':False
     },
     {
         'id':'factorial',
         'title': 'Factorial',
-        'description': 'Return n factorial.',
+        'description': 'Returns n factorial.',
         'done':False
     }
 ]
 
 @app.errorhandler(404) # handles 404 errors
 def notFound(error):
-    return make_response(jsonify({'404 error': 'Page not found.'}), 404)
+    return make_response(jsonify({'error': '404: Page not found.'}), 404)
 
-@app.route('/tasks', methods=['GET']) # returns list of tasks
-def get_tasks():
+@app.route('/tasks', methods=['GET'])
+def get_tasks():  # returns list of all tasks
     return jsonify({'tasks': tasks})
 
-@app.route('/<str:taskID>', methods=['GET']) # checks to see if task exists before calling it
-def getTask(taskID):
+@app.route('/<string:taskID>', methods=['GET'])
+def getTask(taskID): # returns info on a specific tasks
     task = [task for task in tasks if task['id'] == taskID]
     if len(task) == 0:
         abort(404)
     return jsonify({'task':task[0]})
 
-@app.route('/md5/<str:string>')
+@app.route('/md5/<string:string>')
 def getMD5(string):
     md5Hash = hashlib.sha224(str(string).encode('utf-8')).hexdigest()
     return render_template('index.html', output=md5Hash)

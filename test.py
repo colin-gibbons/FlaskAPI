0c9a43922f43068fc633f31bf724e3dfa0fbcdbe
Casandra Ramos <cassy91ramos@tamu.edu>
2018-03-29 11:08:17 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-
+#
+Not working code
+ef setUpClass(cls):
+    install( level='DEBUG' )
+    host = 'localhost'
+    # here you can edit your base url
+    cls.ApiUrl = "http://localhost:8000/"
+    # here you can edit your database connection credentials
+    cls.db = connect( host=host,
+                      user='username',
+                      password='password',
+                      database='password' )
+@classmethod
+def tearDownClass(cls):
+    cls.db.close( )
+    print("------test is over------")
+  

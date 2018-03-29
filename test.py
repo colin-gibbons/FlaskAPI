f803c54c1fd791fe6ff379a6a91959e8f43cd09c
ealvarado150 <36276020+ealvarado150@users.noreply.github.com>
2018-03-29 11:09:55 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 #
+
 Not working code
 ef setUpClass(cls):
     install( level='DEBUG' )
     host = 'localhost'
     # here you can edit your base url
+    
     cls.ApiUrl = "http://localhost:8000/"
     # here you can edit your database connection credentials
+    
     cls.db = connect( host=host,
                       user='username',
                       password='password',

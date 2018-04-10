27778869441c350a452af2533ee09eab153b370d
Nicholas Beach <nicholasallenbeach@gmail.com>
2018-04-10 11:40:32 -0500

    slack part

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-###!/usr/bin/python
+#!/usr/bin/python
 import click
 import requests
 
@@ -16,16 +16,27 @@ import requests
 @click.command()
 
 @click.argument('src', nargs=1)
-@click.argument('dst', nargs=1)
+@click.argument('dst', nargs=-1)
 
 def tcmg476(src, dst):
+    blah = ''
     url = 'http://35.185.41.112/'
     url=url+src+'/'
-    url=url+dst
+    
+    if len(dst)>1:
+        for i in dst:
+            blah = blah+str(i)+' '
+        dst =blah
+        url=url+dst
+    else:
+        for i in dst:
+            dst = str(i)
+            url=url+dst
     
     print(url)
     r = requests.get(url)
     print(r.content)
+    
 
 
 if __name__ == '__main__':
@@ -34,6 +45,10 @@ if __name__ == '__main__':
     url = 'http://35.185.41.112/'
     tcmg476()
 
+
+    
+
+    
 
     
 

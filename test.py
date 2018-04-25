a63c46ec49cf462cbebf8ea149bda1e7a9f22828
kingcassy <cassy9qramos@tamu.edu>
2018-04-25 17:57:31 -0500

    code changes

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ HASH_1 = '098f6bcd4621d373cade4e832627b4f6'   # 'test'
 HASH_2 = 'db0f6f37ebeb6ea09489124345af2a45'   # 'group'
 FIB_SEQ = [0,1,1,2,3,5,8,13,21,34]
 HTTP_ENCODE = "Sorry%20about%20the%20alerts..."
-print "Testing API for expected results...\n"
+print ("Testing API for expected results...\n")
 tests = {
     '/md5/test':                    (200, HASH_1),
     '/md5/group':           (200, HASH_2),
@@ -29,21 +29,21 @@ tests = {
 FAILED = 0
 PASSED = 0
 for uri, test_result in tests.items():
-    print " * ", uri, "... ",
+    print(" * ", uri, "... ", end=" ")
     resp = requests.get('http://www.aslancole.com'+uri)
     if resp.status_code == test_result[0]:
         if test_result[1] == None or resp.json()['output'] == test_result[1]:
-            print "Congrats it Passed"
+            print("Congrats it Passed")
             PASSED += 1
         else:
-            print "FAILED"
-            print "          - Expected output: '%s'" % str(test_result[1])
-            print "          - Actual output:   '%s'" % str(resp.json()['output'])
+            print("FAILED")
+            print("          - Expected output: '%s'" % str(test_result[1]))
+            print("          - Actual output:   '%s'" % str(resp.json()['output']))
             FAILED += 1
     else:
-        print "FAILED"
-        print "          - Expected HTTP status: %i" % test_result[0]
-        print "          - Actual HTTP status:   %i" % resp.status_code
+        print("FAILED")
+        print("          - Expected HTTP status: %i" % test_result[0])
+        print ("          - Actual HTTP status:   %i" % resp.status_code)
         FAILED += 1
 rate = float(PASSED) / float(FAILED+PASSED) * 100.0
-print "\n\n Passed %i of %i tests (%i%% Success rate)" % (PASSED, FAILED+PASSED, rate)
+print ("\n\n Passed %i of %i tests (%i%% Success rate)" % (PASSED, FAILED+PASSED, rate))

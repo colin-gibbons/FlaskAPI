6301108240783e7076bafe1f2a9044a9585810fd
Nicholas Beach <nicholasallenbeach@gmail.com>
2018-04-26 04:22:45 -0500

    updated code

--- test.py
+++ test.py
@@ -8,11 +8,10 @@ HASH_2 = 'db0f6f37ebeb6ea09489124345af2a45'   # 'group'
 FIB_SEQ = [0,1,1,2,3,5,8,13,21,34]
 HTTP_ENCODE = "Sorry%20about%20the%20alerts..."
 print ("Testing API for expected results...\n")
-kvtests = {
-   '/kv-record/Immalittle teepot':    (200,False), 
-}
+
    
 tests = {
+    '/kv-record/Immalittle teepot':    (200,False),
     '/kv-retrieve/Immalittle':            (500, None),
     '/md5/test':                    (200, HASH_1),
     '/md5/group':           (200, HASH_2),
@@ -36,30 +35,30 @@ tests = {
     }
 FAILED = 0
 PASSED = 0
-for uri, test_result in kvtests.items(): 
-    print(" * ", uri, "... ", end=" ")
-    payload = {'Immalittle': 'teepot'}
-    kv_url = ('http://35.202.79.180/kv-record')
-    r = requests.post(kv_url, data=payload)
-    requests.post(kv_url, data=payload)
-   # resp = requests.get('http://35.202.79.180/kv-record')
-    #print(r.json())
-    #print(r.content)
-    if r.status_code == test_result[0]:
-       
-        if test_result[1] == None or r.json()['error'] == test_result[1] or r.json()['output'] == test_result[1]:
-            print("Congrats it Passed")
-            PASSED += 1
-        else:
-            print("FAILED")
-            print("          - Expected output: '%s'" % str(test_result[1]))
-            print("          - Actual output:   '%s'" % str(r.json()['output']))
-            FAILED += 1
+
 for uri, test_result in tests.items():
     print(" * ", uri, "... ", end=" ")
     #resp = requests.get('http://www.aslancole.com'+uri)
     resp = requests.get('http://localhost:5000'+uri)
-    if resp.status_code == test_result[0]:
+    if uri == '/kv-record/Immalittle teepot':
+        print(" * ", uri, "... ", end=" ")
+        payload = {'Immalittle': 'teepot'}
+        kv_url = ('http://35.202.79.180/kv-record')
+        r = requests.post(kv_url, data=payload)
+        requests.post(kv_url, data=payload)
+
+        if r.status_code == test_result[0]:
+       
+            if test_result[1] == None or r.json()['error'] == test_result[1] or r.json()['output'] == test_result[1]:
+                print("Congrats it Passed")
+                PASSED += 1
+            else:
+                print("FAILED")
+                print("          - Expected output: '%s'" % str(test_result[1]))
+                print("          - Actual output:   '%s'" % str(r.json()['output']))
+                FAILED += 1
+
+    elif resp.status_code == test_result[0]:
         
         if test_result[1] == None or resp.json()['output'] == test_result[1]:
             print("Congrats it Passed")
